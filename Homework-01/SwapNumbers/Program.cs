ab72125a950a6e34a1d7e7b4401463ea53f6c61a
Ljubomir Joldashev <[200~ljupchejoldashev@yahoo.comgit config --global user.email ljupchejoldashev@yahoo.comgit config --global user.email [200~ljupchejoldashev@yahoo.com>
2023-03-04 08:57:00 +0100

    Changes

--- Homework-01/SwapNumbers/Program.cs
+++ Homework-01/SwapNumbers/Program.cs
@@ -1,4 +1,23 @@
-﻿// Ја стварно не ја разбирам оваа домашна, ова ли се бара ? :)
+﻿// Еве го , ја откривме Америка :D
+
+Console.WriteLine("Enter a frist number:");
+double num1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a second number:");
+double num2 = double.Parse(Console.ReadLine());
+
+double temp = num1;
+num1 = num2;
+num2 = temp;
+
+Console.WriteLine("After Swapping:");
+Console.WriteLine("First Number: " + num1);
+Console.WriteLine("Second Number: " + num2);
+
+Console.ReadLine();
+
+
+// Ја стварно не ја разбирам оваа домашна, ова ли се бара ? :)
 
 Console.WriteLine("Enter a frist number:");
 double number1 = double.Parse(Console.ReadLine());
@@ -6,21 +25,11 @@ double number1 = double.Parse(Console.ReadLine());
 Console.WriteLine("Enter a second number:");
 double number2 = double.Parse(Console.ReadLine());
 
+Console.WriteLine("After Swapping:");
 Console.WriteLine("The first number is:" + number2);
 Console.WriteLine("The second number is:" + number1);
 
-Console.ReadLine();
 
-// Или може и вака
-
-Console.WriteLine("Enter four numbers:");
-
-double num1 = double.Parse(Console.ReadLine());
-double num2 = double.Parse(Console.ReadLine());
-double num3 = double.Parse(Console.ReadLine());
-double num4 = double.Parse(Console.ReadLine());
-
-Console.WriteLine("The order of the numbers is" + " " + num4 + " " + num3 + " " + num2 + " " + num1);
 
 
 
